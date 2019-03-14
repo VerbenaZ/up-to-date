0f177f123f7996e4afa99ef72f3328932de3caa5
verbenaZ <verbenasampson201@gmail.com>
2019-03-14 17:35:45 +0100

    add up set of numbers

--- Level_1.js/SumUp.js
+++ Level_1.js/SumUp.js
@@ -1,6 +1,7 @@
 const SumUp = (input) => {
     let sum = 0;
-    for (let index = 0; index < input.length; index++)
+    for (let index = 0;
+         index < input.length; index++)
     {sum = sum + input [index]};
     return sum; 
 }
